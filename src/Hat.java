80c50f858d1307338df82beed057b9b5cf83013c
Gina <ginavila30@icloud.com>
2022-04-04 14:42:59 -0400

    Updated 2 Hat Class

--- src/Hat.java
+++ src/Hat.java
@@ -2,5 +2,6 @@ public class Hat {
     public static void main(String[] args) {
         // Created another Class to practice
         //Adding new lines for practicing purposes
+        //We continue practicing with git
     }
 }
